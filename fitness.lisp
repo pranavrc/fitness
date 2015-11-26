0b15baa0963d5d2d076ebaf17f6796a30fc736d4
Pranav Ravichandran <me@onloop.net>
2015-11-26 17:33:32 -0500

    Evaluate population of trees

--- fitness.lisp
+++ fitness.lisp
@@ -18,9 +18,17 @@
                               operator-list
                               (- max-tree-depth 1)))))))
 
-(defun evaluate-population (population)
-  (mapcar #'eval population)
-
 (defun generate-population (primitives operators max-tree-depth &optional (member-count 1000))
   (loop for args upto (- member-count 1)
         collect (generate-program-tree primitives operators max-tree-depth)))
+
+(defmacro evaluate-tree (tree args repeat)
+  (if (> repeat 0)
+    (macrolet ((program (args) `,(list 'lambda '(&rest args) tree)))
+      (let ((result (funcall (program) args)))
+        (cons result (evaluate-tree tree result (- repeat 1)))))
+    nil))
+
+(defun evaluate-population (population args repeat &optional (repeat 50))
+  (mapcar #'(lambda (tree) (evaluate-tree tree args repeat)) population))
+
